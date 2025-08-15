a8e6c86c3ad1fc163677db1a8fc24a1baa305dcf
Phi-Anh Ly <phly@1-E-20.42heilbronn.de>
2025-08-15 14:43:51 +0200

    final 150825, all checked, must pass

--- handle.c
+++ handle.c
@@ -6,7 +6,7 @@
 /*   By: phly <phly@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 23:27:24 by phly              #+#    #+#             */
-/*   Updated: 2025/08/14 15:04:12 by phly             ###   ########.fr       */
+/*   Updated: 2025/08/15 14:42:09 by phly             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,69 +31,59 @@ int	handle_conversation(char spec, va_list arg)
 	return (0);
 }
 
-// int	ft_printf(const char *format, ...)
-// {
-// 	va_list	arg;
-// 	int		len;
-// 	int		i;
-// 	int		temp;
-
-// 	va_start(arg, format);
-// 	len = 0;
-// 	i = 0;
-// 	temp = 0;
-// 	while (format[i])
-// 	{
-// 		if (format[i] == '%' && format[i + 1])
-// 		{
-// 			temp = handle_conversation(format[i + 1], arg);
-// 			if (temp == -1)
-// 				return (-1);
-// 			len += temp;
-// 			i += 2;
-// 			continue ;
-// 		}
-// 		if (write(1, &format[i], 1) == -1)
-// 		{
-// 			va_end(arg);
-// 			return (-1);
-// 		}
-// 		i++;
-// 		len++;
-// 	}
-// 	va_end(arg);
-// 	return (len);
-// }
-
 int	ft_printf(const char *format, ...)
 {
 	va_list	arg;
 	int		len;
 	int		i;
+	int		temp;
 
 	va_start(arg, format);
 	len = 0;
 	i = 0;
+	temp = 0;
 	while (format[i])
 	{
 		if (format[i] == '%' && format[i + 1])
 		{
-			if (handle_conversation(format[i + 1], arg) == -1)
+			temp = handle_conversation(format[i + 1], arg);
+			if (temp == -1)
 				return (va_end(arg), -1);
-			len += handle_conversation(format[i + 1], arg);
+			len += temp;
 			i += 2;
 			continue ;
 		}
-		if (write(1, &format[i], 1) == -1)
+		if (write(1, &format[i++], 1) == -1)
 			return (va_end(arg), -1);
-		i++;
 		len++;
 	}
-	va_end(arg);
-	return (len);
+	return (va_end(arg), len);
 }
-// int main()
+
+// int	ft_printf(const char *format, ...)
 // {
-// 	ft_printf("\001\002\007\v\010\f\r\n");
-// 	return 0;
+// 	va_list	arg;
+// 	int		len;
+// 	int		i;
+
+// 	va_start(arg, format);
+// 	len = 0;
+// 	i = 0;
+// 	while (format[i])
+// 	{
+// 		if (format[i] == '%' && format[i + 1])
+// 		{
+// 			if (handle_conversation(format[i + 1], arg) == -1)
+// 				return (-1);
+// 			len += handle_conversation(format[i + 1], arg);
+// 			i += 2;
+// 			continue ;
+// 		}
+// 		if (write(1, &format[i], 1) == -1)
+// 			return (va_end(arg), -1);
+// 		i++;
+// 		len++;
+// 	}
+// 	va_end(arg);
+// 	return (len);
 // }
